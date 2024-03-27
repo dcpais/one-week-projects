295a310cbd2a7746e88d7870eb041ea75ad96266
dcpais <dpaismail@gmail.com>
2024-03-27 17:04:01 +1000

    connection working

--- 2-web-chat-room/client/src/App.jsx
+++ 2-web-chat-room/client/src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
+import { io } from "socket.io-client"
+const socket = io.connect("http://localhost:3001")
 
 function App() {
 
-  return <div></div>
+  const sendMessage = () => {
+    socket.emit("")
+  }
+
+  return (
+    <div className="App">
+      <input placeholder="Message..." />
+      <button> Send Message </button>
+    </div>)
 }
 
 export default App
--- 2-web-chat-room/server/index.js
+++ 2-web-chat-room/server/index.js
@@ -7,8 +7,23 @@ const express = require('express');
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    }
+});
 
-io.on("connection", (socket) => {console.log(socket.id)})
+io.on("connection", (socket) => {
+    console.log("Connected: " + socket.id)
+});
+
+server.listen(3001, () => {
+    console.log("Server Started");
+    console.log(server.address())
+});
