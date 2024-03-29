6a149bc3d27e5a84e8673c7f088f125df40bc4cb
dcpais <dpaismail@gmail.com>
2024-03-29 18:23:55 +1000

    Playing around with state manipulation on server and client side

--- 1-pong-online/client/src/App.jsx
+++ 1-pong-online/client/src/App.jsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from 'react'
+import { io } from "socket.io-client"
+import './App.css'
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
 
-  
+  /**
+   * State
+   */
+  const [lobby, setLobby] = useState(0)
+  const [paddlePos, setPaddlePos] = useState(0)
+  const [ballPos, setBallPos] = useState({ x: 0, y: 0});
+  const [enemyPaddlePos, setEnemyPaddlePos] = useState(0);
 
-  return (<div></div>); 
+  /**
+   * Game instructions
+   */
+  const sendPacket = () => {
+    socket.emit("sendPacket", {update: 1});
+  }
+
+  const updateGameState = (state) => {
+    document.getElementById("p1").style.left = `${state.paddlePos}px`;
+    document.getElementById("p2").style.left = `${state.enemyPaddlePos}px`;
+    document.getElementById("ball").style.left = `${state.ballPos* 5}px`;
+    console.log(document.getElementById("ball").style.left)
+  }
+
+  /**
+   * Incoming communications
+   */
+  useEffect(() => {
+    socket.on("receivePacket", (data) => {
+      console.log("reached");
+      setPaddlePos(data.paddlePos);
+      setEnemyPaddlePos(data.paddlePos);
+      setBallPos(data.ballPos);
+      updateGameState(data)
+      console.log(data);
+    })
+  }, [socket])
+
+  /**
+   * Final exported game component
+   */
+  return (
+    <div name="container" className="game">
+      <div id="p1" className="paddle"></div>
+      <div id="ball" className="ball"></div>
+      <div id="p2" className="paddle"></div>
+      <button name="move" onClick={sendPacket}>Move</button>
+    </div>
+  ); 
 
 }
 
--- 1-pong-online/server/index.js
+++ 1-pong-online/server/index.js
@@ -1,3 +1,4 @@
+/** Imports and HTTP Server setup */
 const express = require('express');
 const app = express();
 const http = require("http");
@@ -5,8 +6,9 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 app.use(cors());
-
 const server = http.createServer(app);
+
+/** Initialize socket server */
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
@@ -14,16 +16,39 @@ const io = new Server(server, {
     }
 });
 
-io.on("connection", (socket) => {
-    console.log("Connected: " + socket.id)
-
-    /**
-     * Instruction set to receive from connections
-     */
-    
-});
-
+/**
+ * Set up server listening on the specified port
+ */
 server.listen(3001, () => {
     console.log("Server Started");
     console.log(server.address())
 });
+
+/**
+ * GAME STATE
+ */
+let p1Pos = 0;
+let p2Pos = 0;
+let ballPos = 0;
+
+/**
+ * Instruction set to receive from connections
+ */
+io.on("connection", (socket) => {
+    console.log("Connected: " + socket.id)
+    
+    /** Receving update from the clients to send updates to whole lobby */
+    socket.on("sendPacket", (data) => {
+        console.log("Reached")
+        p1Pos += 1;
+        p2Pos += 1;
+        ballPos += 1;
+        io.emit("receivePacket", {
+            p1Pos,
+            p2Pos,
+            ballPos
+        })
+    })
+
+});
+
